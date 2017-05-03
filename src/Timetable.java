f9af978cce67a9b4c25abca3ee3ce078c8d0a3ab
Adamouization <aj645@bath.ac.uk>
2017-05-04 00:19:24 +0100

    java update

--- src/Timetable.java
+++ src/Timetable.java
@@ -4,43 +4,50 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
+/**
+*
+*/
 public class Timetable {
 
     public static void main(String[] args) throws IOException {
 		
 		// FIELDS
-		JSONParser parser = new JSONParser();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-        String input = null;
-		String myJSON = "";
-		Object obj;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 	// read pipe input
+		StringBuilder sb = new StringBuilder(); 									// build final string storing JSON input
+        String input = null;														// temporarily store buffered reader input
+		String myJSON = "";															// store JSON input
 		
-		// Read clingo pipe input and store in string using StringBuilder
-        System.out.println("start reading json");
-        while( (input = bf.readLine() ) != null ) {
+		
+		// Read clingo pipe input and store it in a string using StringBuilder
+        System.out.println("Start reading JSON input");
+        while((input = bf.readLine()) != null ) {
 			sb.append(input);
         }
 		myJSON = sb.toString();
-        System.out.println("finished reading json successfully");
+        System.out.println("Successfully read/stored JSON inptut");
 		
 		// parse the input
+		JSONParser parser = new JSONParser();
 		try{
-			obj = parser.parse(myJSON);
-			JSONArray jsonArray = (JSONArray)obj;
-			for (int i = 0; i < jsonArray.size(); i++) {
-				JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
-				//String name = (String) jsonObjectRow.get("Name");
-				//String address = (String) jsonObjectRow.get("Address");
-				//Long telephone = (Long) jsonObjectRow.get("Phone_Number");
-				System.out.println(jsonObjectRow.get(1));
-			}
+			Object obj = parser.parse(myJSON);
+			//JSONArray array = (JSONArray)obj;
+			JSONArray array = (JSONArray)obj;
+			System.out.println(array.get(0));
+			
+			JSONObject obj2 = (JSONObject)array.get(1);
+			
+			//System.out.println(array.get(1));
 		}
-		catch(ParseException pe){
-			//System.err.println("error position: " + pe.getPosition());
-			//System.err.println(pe);
-			pe.printStackTrace();
+		catch (ParseException pe){
+			System.err.println("position: " + pe.getPosition());
+			System.err.println(pe);
 		}
-		System.out.println("end");
     }
 }
+
+/*
+JSONParser parser = new JSONParser();
+Object obj  = parser.parse(content);
+JSONArray array = new JSONArray();
+array.add(obj);
+*/
